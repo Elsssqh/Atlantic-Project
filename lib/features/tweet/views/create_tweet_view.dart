8e541a362db8c4ea91ccb568051cf8445882ec2c
wicheesee <wkhaerunnisyah@gmail.com>
2023-10-07 18:35:03 +0700

    update hastag_view

---
+++ lib/features/tweet/views/create_tweet_view.dart
@@ -0,0 +1,193 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:atlanticc/common/loading_page.dart';
+import 'package:atlanticc/common/rounded_small_button.dart';
+import 'package:atlanticc/constants/assets_constants.dart';
+import 'package:atlanticc/core/utils.dart';
+import 'package:atlanticc/features/auth/controller/auth_controller.dart';
+import 'package:atlanticc/features/tweet/controller/tweet_controller.dart';
+import 'package:atlanticc/theme/pallete.dart';
+import 'package:file_picker/file_picker.dart';
+
+class CreateTweetScreen extends ConsumerStatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const CreateTweetScreen(),
+      );
+  const CreateTweetScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _CreateTweetScreenState();
+}
+
+class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+  final tweetTextController = TextEditingController();
+  List<File> images = [];
+  List<File> file_picker = [];
+  PlatformFile? pickedfile;
+  File? fileToDisplay;
+
+  @override
+  void dispose() {
+    super.dispose();
+    tweetTextController.dispose();
+  }
+
+  void shareTweet() {
+    ref.read(tweetControllerProvider.notifier).shareTweet(
+          images: images,
+          text: tweetTextController.text,
+          context: context,
+          repliedTo: '',
+          repliedToUserId: '',
+        );
+    Navigator.pop(context);
+  }
+
+  void onPickImages() async {
+    images = await pickImages();
+    setState(() {});
+  }
+
+  void onPickFiles() async {
+    // Lets the user pick one file; files with any file extension can be selected
+    String? _fileName;
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(type: FileType.any);
+
+    if (result != null) {
+      _fileName = result!.files.first.name;
+      pickedfile = result!.files.first;
+    } else {
+      // User canceled the picker
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+    final isLoading = ref.watch(tweetControllerProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close, size: 30),
+        ),
+        actions: [
+          RoundedSmallButton(
+            onTap: shareTweet,
+            label: 'Blues',
+            backgroundColor: Color.fromARGB(170, 123, 147, 244),
+            textColor: Pallete.whiteColor,
+          ),
+        ],
+      ),
+      body: isLoading || currentUser == null
+          ? const Loader()
+          : SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(currentUser.profilePic),
+                          radius: 30,
+                        ),
+                        const SizedBox(width: 15),
+                        Expanded(
+                          child: TextField(
+                            controller: tweetTextController,
+                            style: const TextStyle(
+                              fontSize: 25,
+                            ),
+                            decoration: const InputDecoration(
+                              hintText: "What's happening right now?",
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                            maxLines: null,
+                          ),
+                        ),
+                      ],
+                    ),
+                    if (images.isNotEmpty)
+                    
+                      CarouselSlider(
+                        items: images.map(
+                          (file) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 5,
+                              ),
+                              child: Image.file(file),
+                            );
+                          },
+                        ).toList(),
+                        options: CarouselOptions(
+                          height: 400,
+                          enableInfiniteScroll: false,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Color.fromARGB(218, 79, 89, 135),
+              width: 1,
+
+            ),
+          ),
+        ),
+
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 10,
+                right: 10,
+              ),
+
+              child: GestureDetector(
+                onTap: onPickImages,
+                child: SvgPicture.asset(AssetsConstants.aticon),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 10,
+                right: 10,
+                bottom: 12,
+              ),
+
+              child: GestureDetector(
+                onTap: onPickFiles,
+                child: SvgPicture.asset(AssetsConstants.gifIcon),
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
