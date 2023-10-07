1930730f14b905f52d19df6a9c10b7bde9d7581d
unknown <merrymanurung912@gmail.com>
2023-10-07 19:43:54 +0700

    update rounded_small_button

---
+++ lib/common/rounded_small_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:Atlantic/theme/pallete.dart';
+
+class RoundedSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+  const RoundedSmallButton({
+    super.key,
+    required this.onTap,
+    required this.label,
+    this.backgroundColor = const Color.fromARGB(255, 100, 125, 210),
+    this.textColor = Pallete.whiteColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Chip(
+        label: Text(
+          label,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 16,
+          ),
+        ),
+        backgroundColor: backgroundColor,
+        labelPadding: const EdgeInsets.symmetric(
+          horizontal: 27,
+          vertical: 7,
+        ),
+      ),
+    );
+  }
+}
