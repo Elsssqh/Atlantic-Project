2f3cda54377a640c72e72cd6220c0367d3d13755
535220056 <143670021+535220056@users.noreply.github.com>
2023-10-07 13:17:55 +0700

    update assets_constants

---
+++ lib/theme/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color backgroundColor = Colors.black;
+  static const Color searchBarColor = Color.fromRGBO(32, 35, 39, 1);
+  static const Color blueColor = Color.fromRGBO(29, 155, 240, 1);
+  static const Color whiteColor = Colors.white;
+  static const Color greyColor = Colors.grey;
+  static const Color redColor = Color.fromRGBO(249, 25, 127, 1);
+  static const Color darkblueColor = Color.fromRGBO(6, 25, 85, 1);
+  static const Color purpleColor = Color.fromRGBO(129, 56, 202, 1);
+}
