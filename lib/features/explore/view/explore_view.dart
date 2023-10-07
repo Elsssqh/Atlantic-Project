f4d3bde45d68ed4d873691175b2a17900ca571ee
unknown <merrymanurung912@gmail.com>
2023-10-07 19:59:51 +0700

    update explore_view

---
+++ lib/features/explore/view/explore_view.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:Atlantic/common/error_page.dart';
+import 'package:Atlantic/common/loading_page.dart';
+import 'package:Atlantic/features/explore/controller/explore_controller.dart';
+import 'package:Atlantic/features/explore/widgets/search_tile.dart';
+import 'package:Atlantic/theme/pallete.dart';
+
+class ExploreView extends ConsumerStatefulWidget {
+  const ExploreView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends ConsumerState<ExploreView> {
+  final searchController = TextEditingController();
+  bool isShowUsers = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appBarTextFieldBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(50),
+      borderSide: const BorderSide(
+        color: Color.fromARGB(255, 105, 146, 201),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 70,
+          child: TextField(
+            controller: searchController,
+            onSubmitted: (value) {
+              setState(() {
+                isShowUsers = true;
+              });
+            },
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(10).copyWith(
+                left: 20,
+              ),
+              fillColor: Color.fromARGB(255, 147, 189, 244),
+              filled: true,
+              enabledBorder: appBarTextFieldBorder,
+              focusedBorder: appBarTextFieldBorder,
+              hintText: 'Search Atlantic',
+            ),
+          ),
+        ),
+      ),
+      body: isShowUsers
+          ? ref.watch(searchUserProvider(searchController.text)).when(
+                data: (users) {
+                  return ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final user = users[index];
+                      return SearchTile(userModel: user);
+                    },
+                  );
+                },
+                error: (error, st) => ErrorText(
+                  error: error.toString(),
+                ),
+                loading: () => const Loader(),
+              )
+          : const SizedBox(),
+    );
+  }
+}
