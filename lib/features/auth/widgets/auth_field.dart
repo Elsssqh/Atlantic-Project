e94a935e0dfd63b944ba451f95fd2a5979151678
Jecheytaso <jecheytaso@gmail.com>
2023-10-08 18:30:16 +0700

    auth_field

--- lib/features/auth/widgets/auth_field.dart
+++ lib/features/auth/widgets/auth_field.dart
@@ -1,42 +1,118 @@
+import 'package:atlanticc/theme/palette.dart';
 import 'package:flutter/material.dart';
 
-class AuthField extends StatelessWidget {
+ 
+
+class AuthField extends StatefulWidget {
   final TextEditingController controller;
   final String hintText;
+  final String titleText;
+  final bool isPassword;  
+  final Color backgroundColor;
+
   const AuthField({
+
     super.key,
     required this.controller,
     required this.hintText,
+    required this.titleText,
+    required this.isPassword,
+    this.backgroundColor=Colors.blue,
+
   });
 
-  
+ 
+
+  @override
+  State<AuthField> createState() => _AuthFieldState();
+}
+
+ 
+
+class _AuthFieldState extends State<AuthField> {
+  bool _showPassword = false;
+
+  void _togglevisibility() {
+    setState(() {
+      _showPassword = !_showPassword;
+    });
+
+  }
+
+ 
+
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: Color.fromARGB(255, 76, 100, 177),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(7),
-          borderSide: const BorderSide(
-            color: Color.fromARGB(255, 145, 172, 187),
-            width: 2,
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Text(
+          widget.titleText,
+          style: TextStyle(
+            fontSize: 20,
+
           ),
+
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(7),
-          borderSide: const BorderSide(
-            color: Color.fromARGB(255, 76, 100, 177),
-            width: 2,
-          ),
-        ),
-        contentPadding: const EdgeInsets.all(22),
-        hintText: hintText,
-        hintStyle: const TextStyle(
-          fontSize: 18,
-        ),
+
       ),
-    );
+
+      const SizedBox(height: 7),
+      TextFormField(
+        controller: widget.controller,
+        obscureText: widget.isPassword ? !_showPassword : false,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: const BorderSide(
+              color:  Color.fromARGB(255, 91, 124, 202),
+              width: 2,
+
+            ),
+
+          ),
+
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: const BorderSide(
+              color:  Color.fromARGB(255, 91, 124, 202),
+              width: 1,
+            ),
+
+          ),
+
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(
+            fontSize: 18,
+
+          ),
+
+          fillColor:  Color.fromARGB(255, 91, 124, 202),
+          filled:true,
+          suffixIcon: GestureDetector(
+            onTap: () {
+              widget.isPassword ? _togglevisibility() : DoNothingAction();
+
+            },
+
+            child: Icon(
+              _showPassword ? Icons.visibility : Icons.visibility_off,
+              color: widget.isPassword ? Colors.black : Colors.transparent,
+
+            ),
+
+          ),
+
+        ),
+
+      ),
+
+    ]);
+
   }
+
 }
+
